9a1c0866f3482a312100055349f7969e98759349
charity0721 <3307369267@qq.com>
2026-09-12 17:21:24 +0800

    添加记账原始数组数据

---
+++ calc-tool/script.js
@@ -0,0 +1,8 @@
+const bills = [
+    { name:"午餐", money:16, type:"餐饮" },
+    { name:"公交", money:2, type:"交通" },
+    { name:"零食", money:-8, type:"餐饮" },
+    { name:"网购", money:120, type:"购物" },
+    { name:"饮水", money:0, type:"日用" }
+];
+console.table(bills);
