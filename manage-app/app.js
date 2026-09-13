c2424f84074034ae01d8133f2ecffa70c6bbd22f
charity0721 <3307369267@qq.com>
2026-09-13 15:32:27 +0800

    增加图书删除功能

--- manage-app/app.js
+++ manage-app/app.js
@@ -7,25 +7,30 @@ const listWrap = document.querySelector('#listWrap');
 
 let bookList = [];
 
-//渲染
 function render(){
     listWrap.innerHTML = '';
     bookList.forEach((item,idx)=>{
         const div = document.createElement('div');
         div.className = "item";
-        div.textContent = `书名：${item.name}｜作者：${item.author}｜评分：${item.score}分`;
+        div.textContent = `书名：${item.name}｜作者：${item.author}｜评分：${item.score}分 `;
+        //删除按钮
+        const delBtn = document.createElement('button');
+        delBtn.textContent="删除";
+        delBtn.onclick = ()=>{
+            bookList.splice(idx,1);
+            render();
+        }
+        div.appendChild(delBtn);
         listWrap.appendChild(div);
     })
 }
 
-//新增
 addForm.addEventListener('submit',e=>{
     e.preventDefault();
     tip.textContent = '';
     const nameVal = bookName.value.trim();
     const autVal = author.value.trim();
     const scoVal = Number(score.value);
-    //输入校验
     if(!nameVal || !autVal){
         tip.textContent = "书名、作者不能为空！";
         return;
